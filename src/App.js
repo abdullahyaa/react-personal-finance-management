626781aaa71524ffe7e35adc43e5dfb9da5e0876
Abdullah AY <97941423+abdullahyaa@users.noreply.github.com>
2025-01-23 19:57:18 +0300

    Upload React JS

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button, Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box textAlign="center" marginTop="50px">
+      <Button colorScheme="teal" size="lg">
+        Chakra UI Butonu
+      </Button>
+    </Box>
+  );
+}
+
+export default App;
